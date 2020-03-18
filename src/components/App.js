94af3cf3d0a3e01d90ce732128448c1ad55cd227
Shilpa Soni <shilpassoni@gmail.com>
2020-03-19 01:10:27 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import profile from '../assets/profile.png';
+import Title from './Title';
+
+class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  }
+
+
+    render() {
+      return (
+          <div>
+             <img src={profile} alt='profile' className=""/>
+             <h1>Hello!</h1>
+             <p>My name is Shilpa.</p>
+             <Title />
+             <p>I'm always looking forward to working on meaningful projects.</p>
+        {
+          this.state.displayBio ? (
+            <div>
+              <p>I live in Pune, and code every day.</p>
+              <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
+              <p>Besides coding, I also love music and ramen!</p>
+              <button onClick={this.toggleDisplayBio}>Show less</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={this.toggleDisplayBio}>Read more</button>
+            </div>
+          )
+        }
+        <hr/>
+        <Projects />
+        <hr/>
+        <SocialProfiles />
+        </div>
+      )  
+
+    }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = ({ children }) => {
+    const style = { 
+        display: 'inline-block',
+        margin: 10,
+        marginBottom: 30
+    }
+
+    return (
+        <div>
+          <div>
+            <h3 style={style}><a href='/'>Home</a></h3>
+            <h3 style={style}><a href='/jokes'>Jokes</a></h3>
+          </div> 
+          {children}
+        </div>
+        
+    )
+}
+
+export default Header;
